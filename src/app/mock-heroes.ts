d66115b02d46ff55a399c522b5a9588ce8195211
su-san1129 <deviationvalue1129@icloud.com>
2020-03-04 14:57:49 +0900

    日本名に変更

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: '麗日お茶子' },
+  { id: 12, name: 'オールマイト' },
+  { id: 13, name: '爆豪勝己' },
+  { id: 14, name: '緑谷出久' },
+  { id: 15, name: '轟焦凍' },
+  { id: 16, name: '切島鋭児郎' },
+  { id: 17, name: '飯田天哉' },
+  { id: 18, name: '相澤消太' },
+  { id: 19, name: '上鳴電気' },
+  { id: 20, name: '心操人使' }
 ];
